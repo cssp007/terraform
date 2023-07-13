de70a7c1fc34f59fd32ba2b49c9d3b92601d1ebb
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2023-07-13 11:13:48 +0530

    Update main.tf

--- modules/services/lambda/main.tf
+++ modules/services/lambda/main.tf
@@ -1,17 +1,17 @@
-locals {
-  lambda_zip_locations = "$lambda_zip_locations"
-}
+#locals {
+#  lambda_zip_locations = "$lambda_zip_locations"
+#}
 
 data "archive_file" "welcome" {
   type        = "zip"
   source_file = "${path.module}/welcome.py"
-  output_path = "${local.lambda_zip_locations}"
+  output_path = "$lambda_zip_locations"
 }
 
 resource "aws_lambda_function" "lambda" {
   function_name = "welcome"
-  filename         = "${local.lambda_zip_locations}"
-  source_code_hash = "${filebase64sha256(local.lambda_zip_locations)}"
+  filename         = "$lambda_zip_locations"
+  source_code_hash = "${filebase64sha256(lambda_zip_locations)}"
   role    = aws_iam_role.lambda_role.arn
   handler = "welcome.hello"
   runtime = "python3.7"
