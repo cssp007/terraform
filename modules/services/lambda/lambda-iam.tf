58dc4eebe23afeff5d24dca104ae7fd133e22505
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2023-07-13 09:55:22 +0530

    Update lambda-iam.tf

--- modules/services/lambda/lambda-iam.tf
+++ modules/services/lambda/lambda-iam.tf
@@ -1,10 +1,36 @@
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_policy"
   role = aws_iam_role.lambda_role.id
-  policy = file("iam/lambda-policy.json")
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "Stmt1689154128779",
+        Action = [
+          "logs:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_role"
-  assume_role_policy = file("iam/lambda-assume-policy.json")
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "",
+        Action = [
+          "sts:AssumeRole",
+        ]
+        Effect   = "Allow"
+        Principal = [
+          "Service:lambda.amazonaws.com"
+        ]
+      },
+    ]
+  })
 }
