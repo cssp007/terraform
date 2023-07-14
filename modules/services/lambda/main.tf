a7d0ee6adb73addb6ec05cfbd2f353b27eb26162
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2023-07-14 17:47:34 +0530

    Update main.tf

--- modules/services/lambda/main.tf
+++ modules/services/lambda/main.tf
@@ -1,18 +1,16 @@
-#locals {
-#  lambda_zip_locations = "$lambda_zip_locations"
-#}
-
 data "archive_file" "welcome" {
+  count = length(var.source_file_path)
   type        = "zip"
-  source_file = "${path.module}/${var.source_file_path}"
-  output_path = var.lambda_zip_locations
+  source_file = "${path.module}/${var.source_file_path[count.index]}"
+  output_path = var.lambda_zip_locations[count.index]
 }
 
 resource "aws_lambda_function" "lambda" {
-  function_name = var.function_name
-  filename         = var.lambda_zip_locations
-  source_code_hash = filebase64sha256(var.lambda_zip_locations)
+  count = length(var.source_file_path)
+  function_name = var.function_name[count.index]
+  filename         = var.lambda_zip_locations[count.index]
+  source_code_hash = filebase64sha256(var.lambda_zip_locations[count.index])
   role    = aws_iam_role.lambda_role.arn
-  handler = "${var.function_name}.${var.handler_name}"
-  runtime = var.runtime
+  handler = "${var.function_name[count.index]}.${var.handler_name[count.index]}"
+  runtime = var.runtime[count.index]
 }
