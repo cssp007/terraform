da1b0fa076719bee7dd417d9f4d985ed08dc9a1a
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2023-07-14 14:34:58 +0530

    Update variables.tf

--- modules/services/lambda/variables.tf
+++ modules/services/lambda/variables.tf
@@ -1,4 +1,15 @@
-lambda_zip_locations
-source_file_path
-function_name
-handler_name
+variable "lambda_zip_locations" {
+  type = string
+}
+
+variable "source_file_path" {
+  type = string
+}
+
+variable "function_name" {
+  type = string
+}
+
+variable "handler_name" {
+  type = string
+}
