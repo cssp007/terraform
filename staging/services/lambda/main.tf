245a9c4d9a112b24437e59e5d1bec64aec02552a
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2023-07-14 11:31:00 +0530

    Update main.tf

--- staging/services/lambda/main.tf
+++ staging/services/lambda/main.tf
@@ -13,4 +13,8 @@ provider "aws" {
 
 module "lambda" {
   source = "../../../modules/services/lambda"
+  lambda_zip_locations = var.lambda_zip_locations
+  source_file_path = var.source_file_path
+  function_name = var.function_name
+  handler_name = var.handler_name
 }
